501495c6246a7f0fee9eebd1e3eacbc20299bccd
Ecemglc35 <ecegulec35@gmail.com>
2026-01-06 15:44:01 -0500

    Binary Search

---
+++ CodePractices/BinarySearch.java
@@ -0,0 +1,30 @@
+package CodePractices;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] ints = { 1, 2, 3, 4, 7, 9, 11 };
+        System.out.println(binarySearch(ints, 9)); // 5
+        System.out.println(binarySearch(ints, 8)); // -1
+        System.out.println(binarySearch(ints, 1)); // 0
+    }
+
+    // Array should be sorted
+    public static int binarySearch(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while (low <= high) {
+            int middlePosition = (low + high) / 2;
+            int middleNumber = numbers[middlePosition];
+
+            if (target == middleNumber)
+                return middlePosition;
+            if (target < middleNumber) {
+                high = middlePosition - 1;
+            } else {
+                low = middlePosition + 1;
+            }
+        }
+        return -1;
+    }
+}
